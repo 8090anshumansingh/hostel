abd2b471bb0adc988df0f4a91e3a126efbca9ecf
8090anshumansingh <anshumansinghrajawat.2000@gmail.com>
2021-11-14 00:35:08 +0530

    tsssd

--- server-side/server.js
+++ server-side/server.js
@@ -5,7 +5,7 @@ import Cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(Cors());
